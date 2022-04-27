bdb07abec3812a997603012ad3f9599ab13c012e
Roger Consul <rogerconsul@yahoo.com.br>
2022-04-27 19:45:22 -0300

    Resolvido URLs dinamicas na search bar. As APIS agora recebem a url como param

--- src/components/APIs.js
+++ src/components/APIs.js
@@ -1,18 +1,20 @@
-const urlFood = 'www.themealdb.com/api/json/v1/1/search.php?s=';
-const urlDrink = 'www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+// const urlFood = (param) => (`www.themealdb.com/api/json/v1/1/search.php?${param}`);
+// by name = ?s=~name~ // // first letter = ?f=~letra~ // // by id = ?=i~id~ //
+// const urlDrink = (param) => (`www.thecocktaildb.com/api/json/v1/1/search.php?${param}`);
+// Roger aqui. Consegui resolver a URL dinamica passando ela quando fizer a busca, desta forma a função FETCH ja recebe a url correta como parametro
 
-async function fetchFood() {
-  const response = await fetch(urlFood);
+async function fetchFood(param) {
+  console.log(param);
+  const response = await fetch(param);
   const foodResponse = await response.json();
-
   return foodResponse;
 }
 
-async function fetchDrink() {
-  const response = await fetch(urlDrink);
-  const foodResponse = await response.json();
+async function fetchDrink(param) {
+  const response = await fetch(param);
+  const drinkResponse = await response.json();
 
-  return foodResponse;
+  return drinkResponse;
 }
 
 export { fetchFood, fetchDrink };
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import RecipesContext from '../context/RecipesContext';
+
+const S = 's=';
+const F = 'f=';
+
+const searchByNameOrFLetter = (nameOrLetter) => (
+  `www.themealdb.com/api/json/v1/1/search.php?${nameOrLetter}`
+);
+const searchByIngredient = (ingredient) => (
+  `www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
+);
 
 function SearchBar() {
+  const { getFood, getDrink } = useContext(RecipesContext);
+  const [selectedRadio, setSelectedRadio] = useState('ingredient'); // by name = ?s=~name~ // // first letter = ?f=~letra~ // // by id = ?=i~number~ // // ingredient i=~string~
+  const [inputValue, setInputValue] = useState('');
+  const areYouFoodPage = useHistory().location.pathname.includes('foods');
+
+  const URLGenerator = (param) => {
+    if (selectedRadio === 'ingredient') {
+      return searchByIngredient(param);
+    }
+    if (selectedRadio === 'name') {
+      return searchByNameOrFLetter(S.concat(param));
+    }
+    if (selectedRadio === 'firstLetter') {
+      return searchByNameOrFLetter(F.concat(param));
+    }
+  };
+
   return (
     <div>
       <input
         type="text"
         placeholder="Search Recipe"
         data-testid="search-input"
+        onChange={ ({ target }) => setInputValue(target.value) }
       />
       <label htmlFor="ingredient-search-radio">
         Ingredient
@@ -16,6 +46,7 @@ function SearchBar() {
           data-testid="ingredient-search-radio"
           id="ingredient-search-radio"
           name="search-radio"
+          onChange={ () => setSelectedRadio('ingredient') }
         />
       </label>
       <label htmlFor="name-search-radio">
@@ -25,7 +56,7 @@ function SearchBar() {
           data-testid="name-search-radio"
           id="name-search-radio"
           name="search-radio"
-
+          onChange={ () => setSelectedRadio('name') }
         />
       </label>
       <label htmlFor="first-letter-search-radio">
@@ -35,11 +66,17 @@ function SearchBar() {
           data-testid="first-letter-search-radio"
           id="first-letter-search-radio"
           name="search-radio"
+          onChange={ () => setSelectedRadio('firstLetter') }
         />
       </label>
       <button
         type="button"
         data-testid="exec-search-btn"
+        onClick={
+          areYouFoodPage
+            ? () => getFood(URLGenerator(inputValue))
+            : () => getDrink(URLGenerator(inputValue))
+        }
       >
         Search
       </button>
