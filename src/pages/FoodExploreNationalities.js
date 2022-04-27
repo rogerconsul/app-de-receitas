5df9407ac93ca06ec6a219c312aaae7305f99d34
VanehSann <vanessacbsantos22@gmail.com>
2022-04-27 09:31:56 -0300

    algumas telas

--- src/pages/FoodExploreNationalities.js
+++ src/pages/FoodExploreNationalities.js
@@ -2,10 +2,14 @@ import React from 'react';
 import HeaderWithSearch from '../components/HeaderWithSearch';
 
 function FoodExploreNationalities() {
+  const nacionalidade = []; // por conta do lint / temporario
   return (
     <>
       <HeaderWithSearch />
       <h1 data-testid="page-title">Explore Nationalities</h1>
+      <select data-testid="explore-by-nationality-dropdown">
+        <option data-testid={ `${nacionalidade}-option` } aria-label="nacionality" />
+      </select>
     </>
   );
 }
