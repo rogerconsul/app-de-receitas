e5b4956eff83d1ae3032ac495ed7ebaea1073a0e
Vitor VM <vitorme.vm@gmail.com>
2022-04-27 12:08:24 -0300

    inserindo API

---
+++ src/components/APIs.js
@@ -0,0 +1,18 @@
+const urlFood = 'www.themealdb.com/api/json/v1/1/search.php?s='
+const urlDrink = 'www.thecocktaildb.com/api/json/v1/1/search.php?s='
+
+async function fetchFood() {
+    const response = await fetch(urlFood);
+    const foodResponse = await response.json();
+
+    return foodResponse;
+}
+
+async function fetchDrink() {
+    const response = await fetch(urlDrink);
+    const foodResponse = await response.json();
+
+    return foodResponse
+}
+
+export { fetchFood, fetchDrink}
--- src/context/RecipesProvider.js
+++ src/context/RecipesProvider.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import RecipesContext from './RecipesContext';
+import { fetchDrink, fetchFood } from '../components/APIs';
 
 function RecipesProvider({ children }) {
   const [email, setEmail] = useState([]);
@@ -10,7 +11,25 @@ function RecipesProvider({ children }) {
     setEmail,
     password,
     setPassword,
+    food,
+    setFood,
+    drink,
+    setDrink,
+    getFood,
+    getDrink,
   };
+
+  const [food,setFood] = useState([]);
+  async function getFood() {
+    const foodResponse = await fetchFood();
+    setFood(foodResponse);
+  }
+
+  const [drink,setDrink] = useState([]);
+  async function getDrink() {
+    const drinkResponse = await fetchDrink();
+    setDrink(drinkResponse);
+  }
 
   return (
     <RecipesContext.Provider value={ contextValue }>
