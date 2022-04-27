32bb487be3ac98ac202f701095246a8bed158fe8
Roger Consul <rogerconsul@yahoo.com.br>
2022-04-27 16:31:58 -0300

    Resolvido o Foods da branch API

--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import Footer from '../components/Footer';
+import React, { useContext, useEffect } from 'react';
 import Header from '../components/Header';
+import RecipesContext from '../context/RecipesContext';
+
+const limite = 12;
 
 function Foods(props) {
+  const { getFood, food } = useContext(RecipesContext);
+
+  useEffect(() => {
+    getFood();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       <Header { ...props } />
       <h1 data-testid="page-title">Foods</h1>
-      <Footer />
+      <div>
+        { food.slice(0, limite).map((meal) => {
+          const { strMealThumb: image, strMeal: name, idMeal: id } = meal;
+          return (
+
+            <>
+              <ul>{name}</ul>
+              <ul>{id}</ul>
+              <ul>{image}</ul>
+              <ul>{meal.name}</ul>
+            </>
+
+          );
+        })}
+
+      </div>
+
     </>
 
   );
