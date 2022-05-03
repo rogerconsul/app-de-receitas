5d3d91de3308fc17bdfd5bbc77c0dd0fe93449e2
VanehSann <vanessacbsantos22@gmail.com>
2022-05-03 07:41:19 -0300

    removendo conflitos

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,8 +1,8 @@
 import React, { useContext, useEffect } from 'react';
+import DrinkCards from '../components/DrinkCards';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import RecipesContext from '../context/RecipesContext';
-import DrinkCards from '../components/DrinkCards';
 
 const limite = 12;
 
@@ -13,7 +13,7 @@ function Drinks(props) {
   useEffect(() => {
     getDrink('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [getDrink]);
   return (
     <>
       <Header { ...props } />
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,9 +1,9 @@
-import React, { useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import RecipesContext from '../context/RecipesContext';
+import React, { useContext, useEffect } from 'react';
 import Cards from '../components/Cards';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import RecipesContext from '../context/RecipesContext';
 
 const limite = 12;
 
@@ -14,7 +14,7 @@ function Foods(props) {
   useEffect(() => {
     getFood('https://www.themealdb.com/api/json/v1/1/search.php?s=');
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [getFood]);
 
   const { meals } = food;
 
