18ca2feb8f8714bd890b06426395c5fd8e8b85fe
VanehSann <vanessacbsantos22@gmail.com>
2022-05-04 07:13:03 -0300

    comitando requisitos no local certo

--- src/pages/DrinkProgress.js
+++ src/pages/DrinkProgress.js
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import RecipesContext from '../context/RecipesContext';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import shareIcon from '../images/shareIcon.svg';
 
 function DrinkProgress() {
   const { getDetailsById, recipeDetails } = useContext(RecipesContext);
@@ -8,6 +10,52 @@ function DrinkProgress() {
     const currentLocation = (window.location.pathname);
     getDetailsById(currentLocation);
   }, [getDetailsById]);
+  const checkedUpdtate = ({ target }) => {
+    if (target.className === 'checked') {
+      console.log(target.parentNode.childNodes);
+      target.className = 'noChecked';
+      return;
+    }
+    target.className = 'checked';
+  };
+  const renderDrinkIngredients = () => {
+    const endIndex = 47;
+    const startIndex = 17;
+    const startMeasurement = 15;
+    const ingredients = Object.values(recipeDetails)
+      .slice(startIndex, endIndex)
+      .filter((item) => item !== null
+    && !(item.includes('.jpg')));
+
+    return ingredients.map((item, index) => {
+      if (item !== null && index < startMeasurement
+        && item !== ''
+        && recipeDetails[`strMeasure${index + 1}`] !== null) {
+        return (
+          <li
+            data-testid={ `${index}-ingredient-step` }
+            key={ index }
+          >
+            <label
+              htmlFor={ `checkbox-ingredient-${index}` }
+              className="noChecked"
+              onClick={ (e) => checkedUpdtate(e) }
+              aria-hidden="true" // lint https://stackoverflow.com/a/64858019
+            >
+              <input
+                // onClick={ (e) => checkedUpdtate(e) }
+                name={ `checkbox-ingredient-${index}` }
+                id={ `checkbox-ingredient-${index}` }
+                type="checkbox"
+              />
+              {`${item} - ${recipeDetails[`strMeasure${index + 1}`]}`}
+            </label>
+          </li>
+        );
+      }
+      return '';
+    });
+  };
 
   return (
     <>
@@ -24,14 +72,20 @@ function DrinkProgress() {
           type="button"
           data-testid="share-btn"
         >
-          share icon
+          <img
+            alt="share recipe"
+            src={ shareIcon }
+          />
         </button>
 
         <button
           type="button"
           data-testid="favorite-btn"
         >
-          favorite icon
+          <img
+            alt="favorite recipe"
+            src={ blackHeartIcon }
+          />
         </button>
       </div>
       <p data-testid="recipe-category">
@@ -41,11 +95,7 @@ function DrinkProgress() {
 
       <div className="ingredients-container">
         <ul>
-          <li
-            data-testid="0-ingredient-name-and-measure"
-          >
-            Ingredients
-          </li>
+          { renderDrinkIngredients() }
         </ul>
       </div>
 
@@ -56,15 +106,11 @@ function DrinkProgress() {
         </p>
       </div>
 
-      <div className="recommended-container">
-        <div data-testid="0-recomendation-card" />
-      </div>
       <button
-        data-testid="start-recipe-btn"
+        data-testid="finish-recipe-btn"
         type="button"
-        className="startRecipeBtn"
       >
-        Start Recipe
+        Finish Recipes
       </button>
     </>
   );
