11f0800d32db20495f042b2b5f564fcd7cb49251
Vitor VM <vitorme.vm@gmail.com>
2022-05-05 12:05:22 -0300

    foi

---
+++ src/components/FoodCategories.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect, useContext } from 'react';
+import RecipesContext from '../context/RecipesContext';
+
+function FoodCategories() {
+  const { food, setFood } = useContext(RecipesContext);
+  const [categories, setCategoriesApi] = useState([]);
+  const [allFood, setAllFood] = useState('');
+  const [select, setSelect] = useState(false);
+  const [selectCategory, setSelectCategory] = useState('');
+
+  useEffect(() => {
+    const getCategoriesApi = async () => {
+      const limite = 5;
+      const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+      const results = await response.json();
+      setCategoriesApi(results.meals.slice(0, limite));
+    };
+    getCategoriesApi();
+  }, []);
+
+  async function fetchCategoriesByFood(param) {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${param}`);
+    const foodCategoriesResponse = await response.json();
+    return foodCategoriesResponse;
+  }
+
+  const categoriesItemsFetch = async ({ target }) => {
+    setAllFood(food);
+    console.log(target.name);
+    if (select === false || selectCategory !== target.name) {
+      const response = await fetchCategoriesByFood(target.name);
+      setFood(response);
+      setSelectCategory(target.name);
+      setSelect(!select);
+    } else if (select === true) {
+      setFood(allFood);
+      setSelect(!select);
+    }
+  };
+
+  const returnAll = () => {
+    setFood(allFood);
+  };
+
+  return (
+    <div>
+      <button
+        data-testid="All-category-filter"
+        onClick={ returnAll }
+        type="button"
+      >
+        All
+      </button>
+      {categories.map((category) => ((
+        <button
+          data-testid={ `${category.strCategory}-category-filter` }
+          name={ category.strCategory }
+          key={ category.strCategory }
+          onClick={ categoriesItemsFetch }
+          type="button"
+        >
+          {category.strCategory}
+        </button>
+      )
+      ))}
+    </div>
+  );
+}
+export default FoodCategories;
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { useContext, useEffect } from 'react';
 import FoodCards from '../components/FoodCards';
+import FoodCategories from '../components/FoodCategories';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import RecipesContext from '../context/RecipesContext';
@@ -22,6 +23,7 @@ function Foods(props) {
     <>
       <Header { ...props } />
       <h1 data-testid="page-title">Foods</h1>
+      <FoodCategories />
       <div className="cardsContainer">
         {meals && meals.slice(0, limite).map((meal, index) => (
           <FoodCards meal={ meal } key={ meal.idMeal } index={ index } />
