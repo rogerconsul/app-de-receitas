92875ff8a2fb2468d7e3f0a7af4636a310fca820
VanehSann <vanessacbsantos22@gmail.com>
2022-05-10 08:50:43 -0300

    57

--- src/pages/RecipesDone.js
+++ src/pages/RecipesDone.js
@@ -16,7 +16,7 @@ function RecipesDone() {
     const limitTimeToRemove = 2000;
     console.log(target);
     setCopiedIt(true);
-    copy(`http://localhost:3000/${type}/${id}`);
+    copy(`http://localhost:3000/${type}s/${id}`);
     setTimeout(() => { setCopiedIt(false); }, limitTimeToRemove);
   };
 
